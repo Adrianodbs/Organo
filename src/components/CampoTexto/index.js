2c8dc90feee9fb6aa52a97f76a0ab833b87b607c
Adriano <adriano_alves_2008@hotmail.com>
2022-07-01 20:37:06 -0300

    controlando o estado

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,17 +1,16 @@
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = props => {
-  let valor = ''
-
   const aoDigitado = evento => {
-    valor = evento.target.value
+    props.aoAlterado(evento.target.value)
   }
 
   return (
     <div className="campo-texto">
       <label>{props.label}</label>
       <input
-        value={valor}
+        value={props.valor}
         onChange={aoDigitado}
         required={props.obrigatorio}
         placeholder={props.placeholder}
