27c6c07d6c2f9cdfc2bb6862f1b35ae9fa0bc48b
Adriano <adriano_alves_2008@hotmail.com>
2022-07-02 14:13:24 -0300

    card de colaborador

---
+++ src/components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="https://github.com/adrianodbs.png" alt="" />
+      </div>
+      <div className="rodape">
+        <h4>Adriano Alves</h4>
+        <h5>Front-end</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
